f592f11363cf36985a651302f607f81d437314aa
Galih Roswandi <galihroswandi25@gmail.com>
2023-02-06 00:37:48 +0700

    Video 12 membuat contact app (bagian 3)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yargs = require('yargs');
-const { simpanContact } = require('./contacts');
+const { simpanContact, listContact, detailContact, deleteContact } = require('./contacts');
 
 // yargs.command('add', 'Menambahkan contact baru', () => { }, (argv) => {
 //     console.log(argv);
@@ -29,7 +29,50 @@ yargs.command({
     handler(argv) {
         simpanContact(argv.nama, argv.email, argv.noHP);
     }
+}).demandCommand();
+
+
+// Menampilkan semua daftar kontak 
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan daftar kontak',
+    handler() {
+        listContact();
+    }
 })
 
+
+// Menampilkan daftar kontak berdasarkan nama
+yargs.command({
+    command: 'detail',
+    describe: 'Menampilkan detail kontak berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        detailContact(argv.nama);
+    }
+})
+
+
+// menghapus kontak berdasarkan nama
+yargs.command({
+    command: 'delete',
+    describe: 'Menghapus kontak berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama Lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        deleteContact(argv.nama);
+    }
+})
 
 yargs.parse();
--- contacts.js
+++ contacts.js
@@ -14,44 +14,85 @@ if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, '[]', 'utf-8');
 }
 
+const loadContact = () => {
+    const file = fs.readFileSync('./data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+    return contacts;
+}
+
 
 const simpanContact = (nama, email, noHp) => {
     const contact = { nama, email, noHp };
-    fs.readFile('./data/contacts.json', 'utf-8', (err, file) => {
-        if (err) throw err;
-        const data = JSON.parse(file);
+    const contacts = loadContact();
 
-        // cek duplikat
-        const duplikat = data.find(contact => contact.nama === nama);
-        if (duplikat) {
-            console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'));
+    // cek duplikat
+    const duplikat = contacts.find(contact => contact.nama === nama);
+    if (duplikat) {
+        console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'));
+        return false;
+    }
+
+    // cek email
+    if (email) {
+        if (!validator.isEmail(email)) {
+            console.log(chalk.red.inverse.bold('Email tidak valid!'));
             return false;
         }
+    }
 
-        // cek email
-        if (email) {
-            if (!validator.isEmail(email)) {
-                console.log(chalk.red.inverse.bold('Email tidak valid!'));
-                return false;
-            }
+    // cek noHP
+    if (!validator.isMobilePhone(noHp, 'id-ID')) {
+        console.log(chalk.inverse.red.bold('No Hp tidak valid!'));
+        return false;
+    }
+
+    contacts.push(contact);
+
+    fs.writeFile('./data/contacts.json', JSON.stringify(contacts), (e) => {
+        if (e) {
+            console.log(e);
         }
-
-        // cek noHP
-        if (!validator.isMobilePhone(noHp, 'id-ID')) {
-            console.log(chalk.inverse.red.bold('No Hp tidak valid!'));
-            return false;
-        }
-
-        data.push(contact);
-
-        fs.writeFile('./data/contacts.json', JSON.stringify(data), (e) => {
-            if (e) {
-                console.log(e);
-            }
-        })
-
-        console.log('Terimakasih Sudah Memasukan Data 😉');
     })
+
+    console.log('Terimakasih Sudah Memasukan Data 😉');
 }
 
-module.exports = { simpanContact };
+
+const listContact = () => {
+    const contacts = loadContact();
+    console.log(chalk.bgBlue.inverse('Daftar Kontak : '));
+    contacts.forEach((contact, index) => {
+        console.log(`${index + 1}. ${contact.nama} - ${contact.noHp}`);
+    });
+}
+
+
+const detailContact = (nama) => {
+    const contacts = loadContact();
+    const contact = contacts.find(contact => contact.nama.toLowerCase() === nama.toLowerCase());
+    if (!contact) {
+        console.log(chalk.red.inverse.bgYellow(`${nama} tidak ditemukan !`));
+        return false;
+    }
+
+    console.log(chalk.bgCyan.inverse.white(contact.nama));
+    console.log(`${contact.noHp}`);
+    if (contact.email) {
+        console.log(`${contact.email}`);
+    }
+}
+
+
+const deleteContact = (nama) => {
+    const contacts = loadContact();
+    const newContact = contacts.filter(contact => contact.nama.toLowerCase() !== nama.toLowerCase());
+
+    if (contacts.length === newContact.length) {
+        console.log(chalk.bgRed.inverse.cyan(`${nama} tidak ditemukan !`));
+        return false;
+    }
+
+    fs.writeFile('./data/contacts.json', JSON.stringify(newContact), 'utf-8', (e) => e);
+    console.log(chalk.bgGreen.inverse.blackBright`${nama} berhasil dihapus ! ✔`);
+}
+module.exports = { simpanContact, listContact, detailContact, deleteContact };
