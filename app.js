f606691d984157209138759b77c83f99f67fda7e
Galih Roswandi <galihroswandi25@gmail.com>
2023-02-09 18:43:47 +0700

    Video-15 Belum selesai

--- app.js
+++ app.js
@@ -2,26 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const loadFile = (path, res) => {
-    res.sendFile(path, { root: __dirname }, (err) => {
-        if (err) {
-            console.log(err);
-            return false;
-        }
-    });
-}
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.status(200);
-    loadFile('./pages/index.html', res);
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    loadFile('./pages/about.html', res);
+    res.render('about');
 })
 
 app.get('/contact', (req, res) => {
-    loadFile('./pages/contact.html', res);
+    res.render('contact');
 })
 
 app.get('/product/:id', (req, res) => {
@@ -36,40 +29,3 @@ app.use('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
-
-// const http = require('http');
-// const fs = require('fs');
-// const port = 3000;
-
-// const loadFile = (path, res) => {
-//     fs.readFile(path, (err, data) => {
-//         if (err) {
-//             res.writeHead(404);
-//             res.write('Error: file not found...');
-//         } else {
-//             res.write(data);
-//         }
-//         res.end();
-//     })
-// }
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-
-//     switch (url) {
-//         case '/about':
-//             loadFile('./about.html', res);
-//             break;
-//         case '/contact':
-//             loadFile('./contact.html', res);
-//             break;
-//         default:
-//             loadFile('./index.html', res);
-//             break;
-//     }
-// });
-
-// server.listen(port, () => {
-//     console.log('Server is listening on port 3000...');
-// });
