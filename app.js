7d422d2cd45f41e15a67709f94c63ec22c837789
Galih Roswandi <galihroswandi25@gmail.com>
2023-02-13 19:04:05 +0700

    Video 15 Express-view-engine SELESAI

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
+
 app.get('/', (req, res) => {
     res.status(200);
-    res.render('index');
+
+    const mahasiswa = [
+        {
+            nama: 'Galih Roswandi',
+            email: 'galihroswandi12@gmail.com'
+        },
+        {
+            nama: 'Otong Surotong',
+            email: 'otong01@gmail.com'
+        },
+        {
+            nama: 'Ucup Markucup',
+            email: 'ucup11@gmail.com'
+        },
+    ]
+
+    res.render('index', {
+        layout: 'layouts/main-layout',
+        nama: 'Galih Roswandi',
+        title: 'Halaman Home',
+        mahasiswa
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        layout: 'layouts/main-layout',
+        title: 'Express | About'
+    });
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+        title: 'Express | Contact'
+    });
 })
 
 app.get('/product/:id', (req, res) => {
@@ -27,5 +57,5 @@ app.use('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on http://localhost:${port}/`);
 })
